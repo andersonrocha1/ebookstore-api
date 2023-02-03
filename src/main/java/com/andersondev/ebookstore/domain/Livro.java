4dea7001be91b697795a6f3c425893cb01ca6da0
andersonrocha1 <anderson.tec.informacao@gmail.com>
2023-02-03 19:45:08 -0300

    Classes de domain: Categoria e Livro, criadas

---
+++ src/main/java/com/andersondev/ebookstore/domain/Livro.java
@@ -0,0 +1,97 @@
+package com.andersondev.ebookstore.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Livro implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String titulo;
+	private String nome_autor;
+	private String descricao;
+	private String texto;
+	
+	private Categoria categoria;
+	
+	public Livro() {
+		
+	}
+
+	public Livro(Long id, String titulo, String nome_autor, String descricao, String texto, Categoria categoria) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.nome_autor = nome_autor;
+		this.descricao = descricao;
+		this.texto = texto;
+		this.categoria = categoria;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getNome_autor() {
+		return nome_autor;
+	}
+
+	public void setNome_autor(String nome_autor) {
+		this.nome_autor = nome_autor;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Livro other = (Livro) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
