e68c215c8a17783d02e72d5eb44718a035433c83
andersonrocha1 <anderson.tec.informacao@gmail.com>
2023-02-03 22:08:21 -0300

    Iniciando as associações entre Categoria e Livro

--- src/main/java/com/andersondev/ebookstore/domain/Livro.java
+++ src/main/java/com/andersondev/ebookstore/domain/Livro.java
@@ -3,16 +3,30 @@ package com.andersondev.ebookstore.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_livro")
 public class Livro implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String titulo;
 	private String nome_autor;
 	private String descricao;
 	private String texto;
 	
+	@ManyToOne
+	@JoinColumn(name = "categoria_id")
 	private Categoria categoria;
 	
 	public Livro() {
