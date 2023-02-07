be0c1ec8933816cf217651d4d25a9fe886012b06
andersonrocha1 <anderson.tec.informacao@gmail.com>
2023-02-07 11:35:43 -0300

    Atualizando Categoria com PUT(UPDATE)

--- src/main/java/com/andersondev/ebookstore/resources/CategoriaResource.java
+++ src/main/java/com/andersondev/ebookstore/resources/CategoriaResource.java
@@ -9,8 +9,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -48,6 +50,16 @@ public class CategoriaResource {
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@PutMapping(value="/{id}")
+	public ResponseEntity<Void> update(@RequestBody CategoriaDTO objDTO, @PathVariable Long id) {
+		
+		Categoria obj = service.fromDTO(objDTO);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+
 	}
 
 }
--- src/main/java/com/andersondev/ebookstore/services/CategoriaService.java
+++ src/main/java/com/andersondev/ebookstore/services/CategoriaService.java
@@ -7,32 +7,52 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.andersondev.ebookstore.domain.Categoria;
+import com.andersondev.ebookstore.dto.CategoriaDTO;
 import com.andersondev.ebookstore.repositories.CategoriaRepository;
 import com.andersondev.ebookstore.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	public List<Categoria> findAll() {
-		
+
 		return repository.findAll();
 	}
-	
-	
+
 	public Categoria findById(Long id) {
-		
-	  Optional<Categoria> obj = repository.findById(id);
-	  return obj.orElseThrow(() -> new ResourceNotFoundException("Objeto não encontrado"));
-	  
+
+		Optional<Categoria> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException("Objeto não encontrado"));
+
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repository.save(obj);
-				
+
 	}
+	
+	public Categoria update(Categoria obj) {
+		Categoria newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		
+		newObj.setNome(obj.getNome());
+		newObj.setDescricao(obj.getDescricao());		
+	}
+
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+
+		return new Categoria(objDTO.getId(), objDTO.getNome(), objDTO.getDescricao());
+
+	}
+
+	
 
 }
