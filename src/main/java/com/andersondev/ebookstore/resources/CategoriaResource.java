9777a8c5278c3fbf362c694042500d77d40dc620
andersonrocha1 <anderson.tec.informacao@gmail.com>
2023-02-11 20:51:41 -0300

    Removendo classe

--- src/main/java/com/andersondev/ebookstore/resources/CategoriaResource.java
+++ src/main/java/com/andersondev/ebookstore/resources/CategoriaResource.java
@@ -23,7 +23,7 @@ import com.andersondev.ebookstore.services.CategoriaService;
 
 import jakarta.validation.Valid;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin("*")
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
